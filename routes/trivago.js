44fe61ad5f8160d0cde2142aaff4607d542d5f0d
march-dave <ysangy@gmail.com>
2017-11-11 17:48:08 -0500

    api change

--- routes/trivago.js
+++ routes/trivago.js
@@ -2,33 +2,32 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 
-router.get('/:query/i/:international', function(req, res) {
-    // Beer.findByIdAndUpdate({ id: req.params.id }, { $set: req.body }, { new: true }, (err, beer) => {
-    //     res.status(err ? 400 : 200).send(err || beer);
-    // });
+router.get('/:query/l/:international', function(req, res) {
 
     // var obj = JSON.parse(res).data;
     // trivago/The best ski/i/us
 
-    res.send(req.params.query + " : " + req.params.international);
+    // res.send(req.params.query + " : " + req.params.international);
     // qs: { q: 'The best ski', l: 'US' }
 
     var q2 = req.params.query;
     var l2 = req.params.international;
 
+    // res.send(req.params.query + " : " + req.params.international);
+
     var options = {
         method: 'GET',
         url: 'http://trv-ps-prod.eu-west-1.elasticbeanstalk.com/v1/interpretations',
-        qs: { q: q2, l: l2 }
+        qs: { q: req.params.query, l: req.params.international, format: 'json' }
     };
 
-    // request(options, function(error, response, body) {
-    //     if (error) throw new Error(error);
+    request(options, function(error, response, body) {
+        if (error) throw new Error(error);
 
-    //     console.log(body);
+        // console.log(body);
 
-    //     res.send(body);
-    // });
+        res.send(body);
+    });
 
 
 });
