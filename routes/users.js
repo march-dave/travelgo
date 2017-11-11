fe08dcc696c9844b112abdb05a0075aa8877f36e
march-dave <ysangy@gmail.com>
2017-11-11 15:36:10 -0500

    travelgo ;9Capi call app

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,15 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
     res.send('respond with a resource');
+
+    var options = {
+        method: 'GET',
+        url: 'http://trv-ps-prod.eu-west-1.elasticbeanstalk.com/v1/interpretations',
+        qs: { q: 'The best ski', l: 'CA' }
+
+    };
+
+
 });
 
 module.exports = router;
