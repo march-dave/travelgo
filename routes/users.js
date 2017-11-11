088ddc754c33b2e6d72ec7de6c95a4ac7b66bd1d
march-dave <ysangy@gmail.com>
2017-11-11 15:40:45 -0500

    update json repose

--- routes/users.js
+++ routes/users.js
@@ -3,14 +3,19 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.send('respond with a resource');
+    // res.send('respond with a resource');
 
     var options = {
         method: 'GET',
         url: 'http://trv-ps-prod.eu-west-1.elasticbeanstalk.com/v1/interpretations',
         qs: { q: 'The best ski', l: 'CA' }
-
     };
+
+    request(options, function(error, response, body) {
+        if (error) throw new Error(error);
+
+        console.log(body);
+    });
 
 
 });
